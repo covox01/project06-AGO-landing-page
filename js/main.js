44602b8cbb2b2b7dc6b876fb1db13ec1c29d6ab5
Dennis <oba.dennis@gmail.com>
2020-05-14 20:46:08 -0400

    Adding a Visitor Info section

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
    const canvas = document.getElementById('myCanvas');
    let img;
    let img2;
+   let img3;
 
    // Variables for window width and height 
    let _w = window.innerWidth;
@@ -31,19 +32,22 @@
       // Painting 2
       img2.x = app.renderer.screen.width / 2;
       img2.y = app.renderer.screen.height / 2;
-      console.log()
+      // Painting 3
+      img3.x = app.renderer.screen.width / 2;
+      img3.y = app.renderer.screen.height / 2;
    }
 
    // Event listener which detect if the window has been resized.
-   // window.addEventListener('resize', resize);
+   window.addEventListener('resize', resize);
 
    // Updates the width and height variable when the window has been resized by the user
    function resize() {
       let _w = window.innerWidth;
       let _h = window.innerHeight;
-      app.stop()
+      // app.stop()
       resizeImg()
       app.renderer.resize(_w, _h);
+      console.log("resized")
    }
 
    // This is where it retains all the texture loaded into memory
@@ -55,6 +59,7 @@
    let loader = PIXI.Loader.shared;
    loader.add("painting1", "assets/painting01-Frith_A_Private_View.jpg")
       .add("painting2", "assets/painting03-painting.jpg")
+      .add("painting3", "assets/painting04.jpg")
       .on("progress", handleLoadProgress)
       .on("load", handleLoadAsset)
       .on("error", handleLoadError)
@@ -76,37 +81,42 @@
    function handleLoadComplete() {
       let texture = loader.resources.painting1.texture;
       let texture2 = loader.resources.painting2.texture;
+      let texture3 = loader.resources.painting3.texture;
 
       // Creates a new sprite with a texture
       img = new PIXI.Sprite(texture);
       img2 = new PIXI.Sprite(texture2);
+      img3 = new PIXI.Sprite(texture3);
 
       // Sets transform origin of img
       img.anchor.x = .5;
       img.anchor.y = .5;
       img2.anchor.x = .5;
       img2.anchor.y = .5;
+      img3.anchor.x = .5;
+      img3.anchor.y = .5;
 
       // Adds the images to the stage (canvas element) using the addChild method
-      app.stage.addChild(img2);
       app.stage.addChild(img);
+      app.stage.addChild(img2);
+      app.stage.addChild(img3);
 
       // Makes sure the texture is updated to the window size
       resizeImg()
 
       // Finally animate is called
-      // animate()
+      animate()
    }
 
    // When animate is called, the images are being animated via gsap and pixi.js gsap plugin
    function animate() {
       var tl = new TimelineMax({})
       tl
-         .from(img, 5, { pixi: { scale: 1.5, alpha: 0 } })
-         .to(img, 3, { pixi: { alpha: 0 } }, "-=2.5")
-         .from(img2, 5, { pixi: { scale: 1.5, alpha: 0 }, transformOrigin: "center center" }, "-=2")
-         .to(img2, 5, { pixi: { scale: .8, alpha: 0 } }, "-=2")
-         .to(img, 5, { pixi: { scale: 1.5, alpha: 1 }}, "-=2")
+         .from(img, 7, { pixi: { scale: 1.5, alpha: 0 } })
+         .to(img, 2, { pixi: { alpha: 0 } }, "-=2")
+         .from(img2, 7, { pixi: { scale: 1.5, alpha: 0 }, x: 800, transformOrigin: "center center" }, "-=2")
+         .to(img2, 7, { pixi: { scale: .8, alpha: 0 } }, "-=2")
+         .from(img3, 7, { pixi: { scale: 1.3, alpha: 0 }, x: 600 }, "-=2")
    }
 
 
@@ -133,7 +143,6 @@
          .to(nav, {duration: .7, opacity: 1, xPercent: 0, ease: Power2.easeOut}, "sync")
          .staggerTo("#info-svg, #calendar-svg, #collection-svg", 1, {opacity: 1, y: 0, ease: Back.easeInOut.config(3)}, .1, "-=.8")
          .to(".linkContainer", {duration: .75, opacity: 1, x: 0, ease: Power2.easeOut}, "-=.9")
-         // .staggerTo(".link-con", 1, {opacity: 1, x: 0, ease: Power2.easeOut})
    }  
 
    function setIntroAnimation(){
@@ -141,11 +150,9 @@
       gsap.set(".navIcons", { clip: "rect(0px, 0px, 0px 0px)" })
       gsap.set(".icon-con", { y: 60, opacity: 0})
       gsap.set(".linkContainer", { opacity: 0, x: -100 })   
-      // gsap.set(".link-con", { opacity: 0, x: -100})
 
-     
       addListeners()
-      TweenMax.delayedCall(.7, animateIntro)
+      TweenMax.delayedCall(.3, animateIntro)
    }
 
    function init(){
